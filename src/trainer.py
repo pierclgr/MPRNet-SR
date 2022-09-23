b9ab983aa38b005a6874dae1ff3f3bb73f879492
Pierpasquale Colagrande <pierpasquale.colagrande@univrses.com>
2022-09-23 23:17:17 +0200

    Added number of steps to log

--- src/trainer.py
+++ src/trainer.py
@@ -206,6 +206,7 @@ class Trainer:
                 self.logger.log("val_ssim", val_ssim, epochs)
                 self.logger.log("best_val_psnr", best_val_psnr, summary=True)
                 self.logger.log("best_val_ssim", best_val_ssim, summary=True)
+                self.logger.log("total_steps", steps, step=epochs)
                 self.logger.log_images(train_sr_hr_comparisons[:self.config.wandb.n_images_to_log],
                                        caption="Left: SR, Right: ground truth (HR)",
                                        name="Training samples", step=epochs)
