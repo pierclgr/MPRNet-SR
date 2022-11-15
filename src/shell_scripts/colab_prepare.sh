82f722eff7ac09de3db4f16cac50a03e57324e6e
Pierpasquale Colagrande <pierpasquale.colagrande@univrses.com>
2022-11-15 16:05:22 +0100

    Update colab_prepare.sh

--- src/shell_scripts/colab_prepare.sh
+++ src/shell_scripts/colab_prepare.sh
@@ -12,6 +12,9 @@ mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/
 mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/Set14.zip /content/MPRNet-SR/data/
 mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/BSD100.zip /content/MPRNet-SR/data/
 mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/Urban100.zip /content/MPRNet-SR/data/
+mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/Manga109.zip /content/MPRNet-SR/data/
+mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/SunHays80.zip /content/MPRNet-SR/data/
+mkdir -p /content/MPRNet-SR/data/ && cp /content/drive/MyDrive/Colab\ Notebooks/ML4CV/historical.zip /content/MPRNet-SR/data/
 echo "Done!"
 
 # unzip dataset zip files
@@ -21,6 +24,9 @@ unzip -qq /content/MPRNet-SR/data/Set5.zip -d /content/MPRNet-SR/data/
 unzip -qq /content/MPRNet-SR/data/Set14.zip -d /content/MPRNet-SR/data/
 unzip -qq /content/MPRNet-SR/data/BSD100.zip -d /content/MPRNet-SR/data/
 unzip -qq /content/MPRNet-SR/data/Urban100.zip -d /content/MPRNet-SR/data/
+unzip -qq /content/MPRNet-SR/data/Manga109.zip -d /content/MPRNet-SR/data/
+unzip -qq /content/MPRNet-SR/data/SunHays80.zip -d /content/MPRNet-SR/data/
+unzip -qq /content/MPRNet-SR/data/historical.zip -d /content/MPRNet-SR/data/
 echo "Done!"
 
 # deleting the copied zip files to free space
@@ -30,4 +36,7 @@ rm /content/MPRNet-SR/data/Set5.zip
 rm /content/MPRNet-SR/data/Set14.zip
 rm /content/MPRNet-SR/data/BSD100.zip
 rm /content/MPRNet-SR/data/Urban100.zip
+rm /content/MPRNet-SR/data/Manga109.zip
+rm /content/MPRNet-SR/data/SunHays80.zip
+rm /content/MPRNet-SR/data/historical.zip
 echo "Done!"
